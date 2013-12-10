687f059a9227c40f2c3c4c2b3b892768f80b0b23
Smithey <ciaran@theunit.us>
2013-12-10 21:55:25 +1100

    Fixed & tested

--- src/com/huskehhh/warpsigns/listeners/WarpSignListener.java
+++ src/com/huskehhh/warpsigns/listeners/WarpSignListener.java
@@ -1,5 +1,6 @@
 package com.huskehhh.warpsigns.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -17,56 +18,60 @@ public class WarpSignListener implements Listener {
         Player p = e.getPlayer();
         String[] lines = e.getLines();
 
+        if (p.hasPermission("warpsigns.create")) {
 
-        if (lines[0].equalsIgnoreCase("[Warp]")) {
-
-            if (p.hasPermission("warpsigns.create")) {
+            if (lines[0].equalsIgnoreCase("[Warp]")) {
 
                 if (!lines[1].isEmpty()) {
-                    e.setLine(0, ChatColor.GREEN + "[Warp]");
+                    e.setLine(0, "§1[Warp]");
                     p.sendMessage(ChatColor.GREEN + "Successfully made a Warp sign.");
                 } else {
                     p.sendMessage(ChatColor.RED + "[WarpSigns] Please state a warp on the second line.");
                     e.setCancelled(true);
                 }
 
-            } else {
-                p.sendMessage(ChatColor.RED + "[WarpSigns] You don't have permission to do that!");
-                e.setCancelled(true);
             }
 
+        } else {
+            p.sendMessage(ChatColor.RED + "[WarpSigns] You don't have permission to do that!");
+            e.setCancelled(true);
         }
 
     }
 
     @EventHandler
     public void clickSign(PlayerInteractEvent e) {
-
         Player p = e.getPlayer();
         Block block = e.getClickedBlock();
-        Material material = block.getType();
-
-        if (material == Material.SIGN || material == Material.SIGN_POST || material == Material.WALL_SIGN) {
-
+        
+        if (block.getType() == Material.SIGN || block.getType() == Material.SIGN_POST || block.getType() == Material.WALL_SIGN) {
             if (block.getState() instanceof Sign) {
-
                 Sign sign = (Sign) block.getState();
                 String[] lines = sign.getLines();
-
-                if (ChatColor.stripColor(lines[0]).equalsIgnoreCase("[Warp]")) {
-
-                    if (p.hasPermission("warpsigns.use.*") || p.hasPermission("warpsign.use." + lines[1])) {
-                        p.performCommand("warp " + lines[1]);
+                
+                if (sign.getLine(0).equalsIgnoreCase("§1[Warp]")) {
+                    if (sign.getLine(3).startsWith("{") && sign.getLine(3).endsWith("}")) {
+                    	String Fperm = sign.getLine(4).replace("{", "");
+                    	String perm = Fperm.replace("}", "");
+                    	if(p.hasPermission("warpsigns.use." + perm) || p.hasPermission("warpsigns.use.*")) {
+                    		Bukkit.getServer().dispatchCommand(p, "warp " + sign.getLine(1));
+                    	} else {
+                    		p.sendMessage(ChatColor.DARK_RED + "You need the permission node " + ChatColor.RED + "warpsigns.use." + perm + ChatColor.DARK_RED + " to use this" + ChatColor.GREEN + " WarpSign" + ChatColor.DARK_RED + "!");
+                    	}
                     } else {
-                        p.sendMessage(ChatColor.RED + "[WarpSigns] You don't have permission to do that!");
+                    	if(p.hasPermission("warpsigns.use")) {
+                    		Bukkit.getServer().dispatchCommand(p, "warp " + sign.getLine(1));
+                    	} else {
+                    		p.sendMessage(ChatColor.DARK_RED + "You don't have permission to use this" + ChatColor.GREEN + " WarpSign" + ChatColor.DARK_RED + "!");
+                    	}
                     }
-
+                    	
+                    }
+                    	
                 }
 
             }
 
         }
-
+    	
     }
-
-}
